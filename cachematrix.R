6430b625bebb80dde62a61c8baf7c060a29a4580
Alex <alex.nazarevych@gmail.com>
2014-09-19 12:45:57 +0300

    FINAL

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions provide functionality for
+## calculating and caching the inverse of the matrix to avoid re-calculation
 
-## Write a short comment describing this function
+## makeCacheMatrix function is a wrapper for matrix that provides
+## four interface functions to set and get the cached value of the
+## inverted matrix and the matrix itself
 
 makeCacheMatrix <- function(x = matrix()) 
 {
-    #test
+    # set default value for inverted matrix
+    invCache <- NULL
+    # getter and setter function for inverted matrix cache
+    getInverted <- function() invCache
+    setInverted <- function(inv) invCache <<- inv
+    # getter and setter function for the matrix itself
+    getMatrix <- function() x
+    setMatrix <- function(m)
+    {
+        x <<- m
+        invCache <<- NULL
+    }
+    # return the list of four above functions
+    list (setInverted = setInverted,
+          getInverted = getInverted,
+          setMatrix = setMatrix,
+          getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function calculates and caches the inverse of the matrix only once
+# other times it returns previously cached value instead of recalculating it again
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+    inv <- x$getInverted()
+    # return the cached inverted matrix if it is not empty(NULL)
+    if (!is.null(inv))
+    {
+        message("getting cached inverted matrix")
+        return(inv)
+    }
+    # otherwise cacluate the inverted matrix
+    m <- x$getMatrix()
+    inv <- solve(m, ...)
+    # cache the calculated inverse of the matrix
+    x$setInverted(inv)
+    ## Return a matrix that is the inverse of 'x'
+    inv
 }
+##test case
+#matrix(1:4,2,2) -> m
+#makeCacheMatrix(m) -> cm
+#cacheSolve(cm)
+#cacheSolve(cm)
